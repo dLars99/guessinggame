1063d71c5347bcf8c1c7907bf06645716bf3b043
David Larsen <davidplarsen@gmail.com>
2020-08-25 10:12:51 -0500

    Complete Phase 3 - four guesses

--- Program.cs
+++ Program.cs
@@ -6,22 +6,26 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess the Secret Number!");
-            string userGuess = Console.ReadLine();
-            CheckGuess(userGuess);
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine("Guess the Secret Number!");
+                string userGuess = Console.ReadLine();
+                CheckGuess(userGuess);
+            }
         }
 
         static void CheckGuess(string guess)
         {
             int userNumber = Convert.ToInt32(guess);
             int theAnswer = 42;
+
             if (userNumber == theAnswer)
             {
                 Console.WriteLine("That is correct! You win!");
             }
             else
             {
-                Console.WriteLine("Incorrect! You lose, you loser!");
+                Console.WriteLine("Incorrect! Try better!");
             }
 
         }
