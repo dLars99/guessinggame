6eafb0810b97630501c17dfd49328e025d7a58b4
David Larsen <davidplarsen@gmail.com>
2020-08-25 10:28:41 -0500

    Complete phases 5 and 6 - Randomize answer and say if too high or too low

--- Program.cs
+++ Program.cs
@@ -6,30 +6,39 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 4; i++)
+            int theAnswer = new Random().Next(1, 100);
+
+            for (int i = 0; i < 4; i++)
             {
-                Console.WriteLine($"Your Guess ({i})>");
+                Console.WriteLine($"{4 - i} guesses left!");
                 Console.WriteLine("Guess the Secret Number!");
                 string userGuess = Console.ReadLine();
-                bool correctGuess = CheckGuess(userGuess);
+                int userNumber = Convert.ToInt32(userGuess);
+
+                bool correctGuess = CheckGuess(userNumber, theAnswer);
                 if (correctGuess)
                     break;
             }
         }
 
-        static bool CheckGuess(string guess)
+        static bool CheckGuess(int guess, int answer)
         {
-            int userNumber = Convert.ToInt32(guess);
-            int theAnswer = 42;
 
-            if (userNumber == theAnswer)
+            if (guess == answer)
             {
                 Console.WriteLine("That is correct! You win!");
                 return true;
             }
             else
             {
-                Console.WriteLine("Incorrect! Try better!");
+                if (guess < answer)
+                {
+                    Console.WriteLine("Too low. Aim higher!");
+                }
+                else if (guess > answer)
+                {
+                    Console.WriteLine("Too high. Bring it down.");
+                }
                 return false;
             }
 
