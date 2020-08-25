af4ec0fc6cd4f81b70f50cd98d66e29a03ba799b
David Larsen <davidplarsen@gmail.com>
2020-08-25 10:50:49 -0500

    Complete Phase 7 - add difficulty levels

--- Program.cs
+++ Program.cs
@@ -7,11 +7,13 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             int theAnswer = new Random().Next(1, 100);
-
-            for (int i = 0; i < 4; i++)
+            int numOfGuesses = ChooseDifficulty();
+            Console.Clear();
+            for (int i = 0; i < numOfGuesses; i++)
             {
-                Console.WriteLine($"{4 - i} guesses left!");
                 Console.WriteLine("Guess the Secret Number!");
+                Console.WriteLine("------------------------");
+                Console.Write($"{numOfGuesses - i} guesses left!  ");
                 string userGuess = Console.ReadLine();
                 int userNumber = Convert.ToInt32(userGuess);
 
@@ -21,9 +23,42 @@ namespace GuessingGame
             }
         }
 
+        static int ChooseDifficulty()
+        {
+            Console.Clear();
+            Console.WriteLine("Please choose a difficulty level:");
+            Console.WriteLine(" (E)asy");
+            Console.WriteLine(" (M)edium");
+            Console.WriteLine(" (H)ard");
+            string difficulty = Console.ReadLine();
+
+            int numOfGuesses = 0;
+            switch (difficulty.ToLower())
+            {
+                case "e":
+                case "easy":
+                    numOfGuesses = 8;
+                    break;
+                case "m":
+                case "medium":
+                case "med":
+                    numOfGuesses = 6;
+                    break;
+                case "h":
+                case "hard":
+                    numOfGuesses = 4;
+                    break;
+                default:
+                    numOfGuesses = 1;
+                    break;
+            }
+
+            return numOfGuesses;
+
+        }
+
         static bool CheckGuess(int guess, int answer)
         {
-
             if (guess == answer)
             {
                 Console.WriteLine("That is correct! You win!");
@@ -38,6 +73,7 @@ namespace GuessingGame
                 else if (guess > answer)
                 {
                     Console.WriteLine("Too high. Bring it down.");
+                    Console.WriteLine();
                 }
                 return false;
             }
